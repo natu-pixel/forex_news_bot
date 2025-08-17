7290fadf6b56ce613a65ea7f42794bb822df0ec4
natu-pixel <natnaelkuzmi25@gmail.com>
2025-08-18 01:44:05 +0300

    Update forex_news_bot.py

--- forex_news_bot.py
+++ forex_news_bot.py
@@ -10,7 +10,7 @@ from flask import Flask
 
 # === CONFIG FROM ENV VARIABLES ===
 BOT_TOKEN = os.environ.get("8318698327:AAHIUm8e3ty2mh8q-Crj7BoqIMyFlg7LcRk")
-CHAT_ID = int(os.environ.get("-1002786950182")) 
+CHAT_ID = int(os.environ.get("CHAT_ID"))
 EMAIL = os.environ.get("natikuzmi@gmail.com")
 PASSWORD = os.environ.get("2Y5drzMD@r@52X5")
 
@@ -99,4 +99,5 @@ schedule_alerts()
 while True:
     schedule.run_pending()
     time.sleep(30)
+
 
